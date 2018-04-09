2a3f5a440352de575d3507a4696173e898f3cfde
usopp <usopp9@naver.com>
2018-04-09 16:52:50 +0900

    회원가입 중

---
+++ src/main/java/com/dgit/handler/LoginHandler.java
@@ -0,0 +1,22 @@
+package com.dgit.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dgit.controller.CommandHandler;
+
+public class LoginHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		
+		if(req.getMethod().equalsIgnoreCase("get")){
+			return "/WEB-INF/view/loginAgreement.jsp";
+		}else if(req.getMethod().equalsIgnoreCase("post")){
+			return "/WEB-INF/view/loginForm.jsp";
+		}
+		return null;
+	}
+
+}
