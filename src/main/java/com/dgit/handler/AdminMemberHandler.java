e4d97165c7155ae497aad45ad11ba9a643ea37a4
usopp <usopp9@naver.com>
2018-04-16 18:03:43 +0900

    회원관리 중

---
+++ src/main/java/com/dgit/handler/AdminMemberHandler.java
@@ -0,0 +1,18 @@
+package com.dgit.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dgit.controller.CommandHandler;
+
+public class AdminMemberHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if(req.getMethod().equalsIgnoreCase("get")){
+			return "/WEB-INF/view/admin_member.jsp";
+		}
+		return null;
+	}
+
+}
