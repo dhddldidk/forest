8bbd601c3b518e837075d6d346b939dbf8534588
ragne <login486@naver.com>
2018-04-22 18:47:47 +0900

    관리자 모드 예약 현황 리스트, 수정까지 완료!

--- src/main/java/com/dgit/handler/AdminHandler.java
+++ src/main/java/com/dgit/handler/AdminHandler.java
@@ -1,16 +1,43 @@
 package com.dgit.handler;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+
 import com.dgit.controller.CommandHandler;
+import com.dgit.dao.ReservationDao;
+import com.dgit.model.Reservation;
+import com.dgit.util.MySqlSessionFactory;
 
 public class AdminHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		// TODO Auto-generated method stub
-		return "/WEB-INF/view/admin.jsp";
+		SqlSession session = null;
+		
+		if(req.getMethod().equalsIgnoreCase("get")){
+
+			try{
+				session = MySqlSessionFactory.openSession();
+				
+				ReservationDao reser = session.getMapper(ReservationDao.class);
+				
+				List<Reservation> list = reser.selectReservationByAll();
+				
+				req.setAttribute("list", list);
+				
+			}catch (Exception e){
+				e.printStackTrace();
+			}finally {
+				session.close();
+			}
+
+			return "/WEB-INF/view/admin.jsp";
+		}
+		return null;
 	}
 
 }
